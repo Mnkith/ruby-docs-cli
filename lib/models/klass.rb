8551849a3e13de9a5701e9af63fe7423f64c9da9
Mnkith <munkithabid@gmail.com>
2020-12-05 21:25:10 -0600

    nesting class and instance modules into scraper

--- lib/models/klass.rb
+++ lib/models/klass.rb
@@ -1,30 +1,39 @@
 require_relative '../scraper.rb'
 class Klass 
-  extend Scraper
+  extend Scraper::ClassMethods
+  include Scraper::InstanceMethods
   @@all_classes_locator = "div#c" + "lass-index div.entries p a" #ruby is a JOKE, it considers class as keyword even inside a string
   @@all_methods_locator = "div#method-list-section ul.link-list li"
-  @@all_classes = get_elements(@@all_classes_locator) 
+  @@all = get_elements(@@all_classes_locator)
 
   # display_content(@@all_classes)
   attr_reader :class_methods, :name, :description
   def initialize(class_name)
-    @name = class_name
-    @class_methods = self.class.get_elements(@@all_methods_locator, @name)
-    @description = self.class.description(self)
+      cn = validate(class_name)
+      if cn 
+      @name = cn
+      @class_methods = self.class.get_elements(@@all_methods_locator, @name)
+      @description = self.class.description(self)
+    else
+      puts "no class"
+    end
   end
 
+  # def is_there?(element)
+
+  # end
   def methods_count
     class_methods.size
   end
 
-  def all_classes 
-    @@all_classes
+  def all
+    @@all
   end
 end
-k = Klass.new("String")
-puts k.description
+k = Klass.new("StRng")
+# puts k.description
 # puts k.class_methods
 # puts k.all_classes
-# Klass.display_content(k.class_methods)
+# Klass.display_content(k.all)
 # puts k.methods_count
 # puts Klass.methods
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,37 +1,53 @@
 require 'nokogiri'
 require 'open-uri'
-module Scraper
+module Scraper 
+  module ClassMethods
   @@main_uri = "https://ruby-doc.org/core-2.7.2/" # not a constant because it's content will change dynamically
   # main_site = open(main_uri)                    # not a constant because it's content will change dynamically
 
-  def get_elements(element_locator, uri_suffix = '')
-    sub_page = open(normalize_uri(uri_suffix))
-    doc = Nokogiri::HTML(sub_page)
-    doc.css(element_locator)
-  end
-  
-  def normalize_uri(suffix = '')
-    return @@main_uri + suffix.gsub(/::/, '/') + '.html' if suffix != ''
-    @@main_uri
+    def get_elements(element_locator, uri_suffix = '')
+      sub_page = open(normalize_uri(uri_suffix))
+      doc = Nokogiri::HTML(sub_page)
+      doc.css(element_locator)
+    end
+    
+    def normalize_uri(suffix = '')
+      return @@main_uri + suffix.gsub(/::/, '/') + '.html' if suffix != ''
+      @@main_uri
+    end
+
+    def description(t)
+      if t.class == Klass
+        get_elements('div#documentation div#description.description', t.name).text
+      else
+        call =  d.css('div#count-method.method-detail span.method-callseq').text
+        details = d.css('div#count-method.method-detail div p').text
+        code = d.css('div#count-method.method-detail div pre').text
+        "#{call}#{details}#{code}"
+      end
+    end
+    def display_content(elements_array)
+      elements_array.each_with_index {|el, i| puts "#{i+1}- #{el.text}"}
+    end
   end
 
-  def description(t)
-    if t.class == Klass
-      get_elements('div#documentation div#description.description', t.name).text
-    else
-      call =  d.css('div#count-method.method-detail span.method-callseq').text
-      details = d.css('div#count-method.method-detail div p').text
-      code = d.css('div#count-method.method-detail div pre').text
-      "#{call}#{details}#{code}"
+  module InstanceMethods
+
+    def is_there?(user_input)
+      searchable = self.all.map{|el| el.text.downcase}
+      searchable.include?(user_input.downcase)
+    end
+
+    def validate(user_input) #return the valid name of method or class based on user input if found other wise nil
+      searchable = self.all.map{|el| return el.text if el.text.downcase == user_input.downcase}
+      nil
     end
   end
 
   # def get_description(type)
   #   if
 
-  def display_content(elements_array)
-    elements_array.each_with_index {|el, i| puts "#{i+1}- #{el.text}"}
-  end
+  
 end
 # css(".class")[2].children[1].text
 # puts doc.css("#method-index")[1]
