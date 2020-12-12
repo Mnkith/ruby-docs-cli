5f7a89accad30f2b2be6f9c71664b08df94779cc
Mnkith <munkithabid@gmail.com>
2020-12-12 11:39:24 -0600

    add required files to environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,12 +1,8 @@
-require "ruby_docs_cli/version"
 require 'nokogiri'
 require 'open-uri'
 require 'colorize'
 
+require_relative "./ruby_docs_cli/version"
 require_relative './models/klass'
 require_relative './models/mmethod'
 require_relative './cli'
-module RubyDocsCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
